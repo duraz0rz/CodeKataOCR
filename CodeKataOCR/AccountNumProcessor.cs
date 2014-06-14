df9fe3e51707417e99b4a403090d633f45cf3401
Marc Vertido <marc.vertido@outlook.com>
2014-06-13 23:03:43 -0400

    Wrote a test...woo

---
+++ CodeKataOCR/AccountNumProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKataOCR
+{
+    public class AccountNumProcessor
+    {
+        public string DetermineAccountNumber(List<string> accountLines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeKataOCRTest/AccountNumProcessorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKataOCR;
+using System.Collections.Generic;
+
+namespace CodeKataOCRTest
+{
+    [TestClass]
+    public class AccountNumProcessorTest
+    {
+        AccountNumProcessor processor;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            processor = new AccountNumProcessor();
+        }
+
+        [TestMethod]
+        public void DetermineAccountNumberReadsZeroCorrectly()
+        {
+            var accountLines = new List<string>() {
+                " _  _  _  _  _  _  _  _  _ ",
+                "| || || || || || || || || |",
+                "|_||_||_||_||_||_||_||_||_|"
+            };
+
+            var accountNum = processor.DetermineAccountNumber(accountLines);
+
+            Assert.AreEqual("000000000", accountNum);
+        }
+    }
+}
